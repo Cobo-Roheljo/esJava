7f532cb7e76f851cc85be960859c50506b3df6e8
Your Name <you@example.com>
2020-12-15 11:22:05 +0100

    esGit

---
+++ EsGit1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esgit1;
+
+/**
+ *
+ * @author witin
+ */
+public class EsGit1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 0; i < 11; i++){
+            if(i < 11){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
